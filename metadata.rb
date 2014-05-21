dc3fb6aaeffd9092c7cb71e67e91ea5c8471149c
Darron Froese <darron@froese.org>
2014-05-21 10:15:08 -0600

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+name             'php5_apache2'
+maintainer       'Darron Froese'
+maintainer_email 'darron@froese.org'
+license          'Apache 2.0'
+description      'Installs/configures php5_apache2'
+version          '0.1.0'
+recipe           'php5_apache2::default', 'Installs/configures php5_apache2'
+
+depends 'chef-sugar'
---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+require 'spec_helper'
+
+# Write integration tests with Serverspec - http://serverspec.org/
+describe 'php5_apache2::default' do
+  it 'does something' do
+    pending 'Replace this with meaningful tests'
+  end
+end
